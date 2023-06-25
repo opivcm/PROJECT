c0e5c2b3e055a34b55f44cebb7a32ed8b70e17fc
OPIVCM <opivcm441@gmail.com>
2023-06-25 15:56:36 +0530

    js

--- EXPLORING/index.js
+++ EXPLORING/index.js
@@ -1,3 +1,8 @@
 console.log('Hello World!');//my first hello world program
 let name='vinay';
-console.log('name')
+console.log(name);
+let firstName='mosh';
+let secondName='bruh';
+console.log(firstName);
+console.log(secondName);
+
